4d3ed756b3d220c9748fd604ff07efc3b5d35326
Tim <46413981+timcodejs@users.noreply.github.com>
2020-12-03 00:07:29 +0900

    app JS Lotto number

---
+++ app.js
@@ -0,0 +1,23 @@
+function lottoNumbers() {
+    const nums = [];
+    for(let i = 0; i < 45; i++) {
+        let randomNum = Math.floor(Math.random() * 45) + 1;
+        if(nums.indexOf(randomNum) == -1) {
+            nums.push(randomNum);
+            if(nums.length == 6) {
+                break;
+            }
+        }
+    }
+    
+    nums.sort(function(a, b){
+        return a - b;
+    });
+    
+    let span = document.querySelectorAll('span');
+    for(let l = 0; l < nums.length; l++) {
+        span[l].append(nums[l]);
+    }
+}
+
+lottoNumbers();
